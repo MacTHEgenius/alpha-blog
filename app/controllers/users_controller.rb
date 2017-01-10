661d154e70b34026fcb48960ebfd0225cd1b98e7
MacTHEgenius <levesque.benoit@me.com>
2017-01-10 01:35:32 -0500

    Create show action and gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,9 +27,9 @@ class UsersController < ApplicationController
   # /users/:id
 
   # GET
-  #def show
-  #
-  #end
+  def show
+    @user = User.find(params[:id])
+  end
 
   # POST - PATCH
   def update
